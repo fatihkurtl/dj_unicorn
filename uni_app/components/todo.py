5567d14218f24d6f06b1fd66df31b43c095b1627
fatihkurtl <kurtf061@gmail.com>
2024-07-29 04:08:00 +0300

    actions warining added

--- uni_app/components/todo.py
+++ uni_app/components/todo.py
@@ -11,6 +11,9 @@ class TodoView(UnicornView):
     task = ""
     todo_items = TodoItem.objects.all()
     form_class = TodoForm
+    create_success_message = ""
+    update_success_message = ""
+    delete_success_message = ""
     
     def add_task(self):
         form = self.form_class({'title': self.task, 'completed': False})
@@ -18,20 +21,21 @@ class TodoView(UnicornView):
             form.save()
             self.todo_items = TodoItem.objects.all()
             self.task = ""
+            self.create_success_message = "Task added successfully"
             
     def update_task(self, task_id):
         task = TodoItem.objects.get(id=task_id)
         task.completed = not task.completed
         task.save()
         self.todo_items = TodoItem.objects.all()
-
+        self.update_success_message = "Task updated successfully"
 
     def delete_task(self, task_id):
         print(task_id)
         TodoItem.objects.get(id=task_id).delete()
         self.todo_items = TodoItem.objects.all()
+        self.delete_success_message = "Task deleted successfully"
         
     def reset(self):
         self.todo_items = TodoItem.objects.all()
         self.task = ""
-        print("Reset method called")
